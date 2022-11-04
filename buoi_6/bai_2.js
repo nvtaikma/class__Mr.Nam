09cadc745949761a277a6a9210f1e39c432435c7
nvtai <nvtai0202@gmail.com>
2022-11-04 21:11:29 +0700

    fd

--- buoi_6/bai_2.js
+++ buoi_6/bai_2.js
@@ -6,21 +6,21 @@ Bài 2: Viết chương trình đệ quy tính tổng các số từ 1 đến n,
 	n=10 -> 55
 */
 // vòng lặp
-function sum(n){
-    var sum1 = 0;
-    for(let i=0; i<=n; i++){
-        sum1 += i;
-    }
-    console.log(sum1); 
-}
-sum(4);
+// function sum(n){
+//     var sum1 = 0;
+//     for(let i=0; i<=n; i++){
+//         sum1 += i;
+//     }
+//     console.log(sum1); 
+// }
+// sum(4);
 
 // đệ quy
 
-// function sumTo(n) {
-//     if (n === 0) return 0;
+function sumTo(n) {
+    if (n === 0) return 0;
   
-//     return n + sumTo(n - 1);
-//   }
-// //   sumTo(10);
-// console.log("tổng là",sumTo(3));
+    return n + sumTo(n - 1);
+  }
+//   sumTo(10);
+console.log("tổng là",sumTo(5));
--- buoi_6/bai_3.js
+++ buoi_6/bai_3.js
@@ -7,17 +7,27 @@ Bài 3: Cho một dãy số [1, 25, 7, -7, -3, 12, -22, 0]
 */
 
 var array = [1, 25, 7, -7, -3, 12, -22, 0,12];
-tangDan =array.sort(function (a, b) {return a - b;});
+
+// 1.
+// tangDan = array.sort(function (a, b) {return a - b;});
 // Nếu giá trị trả về của hàm Function(a,b) < 0, giá trị a sẽ đứng trước b
 // Nếu giá trị trả về = 0, a và b bằng nhau
 // Giá trị trả về > 0, a đứng sau b
-console.log("dãy số theo thứ tự tăng dần",tangDan);
+// console.log("dãy số theo thứ tự tăng dần",tangDan);
 
-function tang(){
-    
-for(var i=0; i<array.length; i++) {
-    // var tangTri =[];
-    console.log("mang i",array[i]);
-}
-}
-tang();
+// 2.
+console.log(array.sort(function(a,b){
+  // return Math.abs(a)-Math.abs(b);
+  if(Math.abs(a) - Math.abs(b) === 0){
+    return a-b;
+  }
+  return Math.abs(a)-Math.abs(b);
+}));
+
+
+
+
+
+
+
+
